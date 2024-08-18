bcbe0678e66851120de8191a51b28696a8b5e0ae
Philipp <philipp.schoeneberg2004@gmx.de>
2024-08-18 11:14:19 +0200

    test

--- sizeFitter.js
+++ sizeFitter.js
@@ -1,7 +1,9 @@
 var main = getElementById("main0");
 var footer = getElementById("footer0");
 
-if(window.innerWidth < 800){
-    main.id = "main1";
-    footer.id = "footer1"
+while(true){
+    if(window.innerWidth < 800){
+        main.id = "main1";
+        footer.id = "footer1"
+    }
 }
