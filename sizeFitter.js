3645967e3d95a817fe2c4db9eab34062d897e484
Philipp <philipp.schoeneberg2004@gmx.de>
2024-08-18 13:06:03 +0200

    Update responsive web design

--- sizeFitter.js
+++ sizeFitter.js
@@ -1,11 +1,13 @@
 var main = document.getElementById("main0");
 var footer = document.getElementById("footer0");
 
-if(window.innerWidth < 800){
-    main.id = "main1";
-    footer.id = "footer1"
-}
-else{
-    main.id = "main0";
-    footer.id = "footer0"
-}
+addEventListener("resize", (event) => {
+    if(window.innerWidth < 800){
+        main.id = "main1";
+        footer.id = "footer1"
+    }
+    else{
+        main.id = "main0";
+        footer.id = "footer0"
+    }
+});
