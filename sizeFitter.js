2847e5e7a8bb71b44c5e065701039f327f548ca3
Philipp <philipp.schoeneberg2004@gmx.de>
2024-08-18 22:48:26 +0200

    Update sizeFitter.js

--- sizeFitter.js
+++ sizeFitter.js
@@ -1,6 +1,15 @@
 var main = document.getElementById("main0");
 var footer = document.getElementById("footer0");
 
+if(window.innerWidth < 800){
+    main.id = "main1";
+    footer.id = "footer1"
+}
+else{
+    main.id = "main0";
+    footer.id = "footer0"
+}
+
 addEventListener("resize", (event) => {
     if(window.innerWidth < 800){
         main.id = "main1";
